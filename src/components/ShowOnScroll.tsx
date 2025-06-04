b65de5b142c10bc5fb3050a469bf5bddf551be9f
TheRealLorenz <lollobellina@gmail.com>
2025-06-04 20:08:43 +0200

    Add ShowOnScroll component

---
+++ src/components/ShowOnScroll.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function ShowOnScroll({ children }: Props) {
+  const [showNavbar, setShowNavbar] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => setShowNavbar(window.scrollY > 0);
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  });
+  return (
+    <div
+      style={{
+        opacity: showNavbar ? 1 : 0,
+      }}
+      className="transition-opacity "
+    >
+      {children}
+    </div>
+  );
+}
