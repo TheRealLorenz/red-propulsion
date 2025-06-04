03f04067a3055e6a0fa9c2092f67c45fd5c850bf
TheRealLorenz <lollobellina@gmail.com>
2025-06-04 22:54:51 +0200

    Fill navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,8 +7,22 @@ export default function Navbar() {
 
   return (
     <ShowOnScroll>
-      <div className="fixed w-full font-2xl font-bold bg-background p-2 z-100">
-        <ReactMarkdown>{content}</ReactMarkdown>
+      <div className="fixed w-full flex bg-background p-2 z-100 items-center shadow">
+        <ReactMarkdown
+          components={{
+            h1: ({ children }) => (
+              <h1 className="text-xl font-bold mr-2">{children}</h1>
+            ),
+            ul: ({ children }) => (
+              <div className="flex items-center"> {children} </div>
+            ),
+            li: ({ children }) => (
+              <div className="mb-0 mt-1 hover:underline mx-2">{children}</div>
+            ),
+          }}
+        >
+          {content}
+        </ReactMarkdown>
       </div>
     </ShowOnScroll>
   );
