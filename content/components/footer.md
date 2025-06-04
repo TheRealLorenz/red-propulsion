77333eb836d2a9628a0f2450f6dbaea63adadd01
TheRealLorenz <lollobellina@gmail.com>
2025-06-04 19:37:37 +0200

    Add links to footer

--- content/components/footer.md
+++ content/components/footer.md
@@ -5,3 +5,6 @@ Dipartimento di Ingegneria Industriale
 Università di Firenze
 
 Email: [info@redpropulsion.it](mailto:info@redpropulsion.it)
+
+- [![](/github-logo-white.svg)](https://github.com/RedPropulsion)
+- [![](/instagram-logo-white.svg)](https://www.instagram.com/red_propulsion/)
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
   const component = Markdown.getComponent("footer.md");
 
   return (
-    <div className="w-full bg-background-dark p-6 text-center md:text-left md:p-12">
+    <div className="w-full bg-background-dark text-background p-6 text-center md:text-left md:p-12">
       <ReactMarkdown
         components={{
           a: ({ children, href }) => (
@@ -15,6 +15,11 @@ export default function Footer() {
           ),
           h1: ({ children }) => (
             <h1 className="text-xl font-bold mb-4">{children}</h1>
+          ),
+          ul: ({ children }) => (
+            <div className="flex justify-center md:justify-start list-none gap-4 m-4 [&>*]:w-10">
+              {children}
+            </div>
           ),
         }}
       >
